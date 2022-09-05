b4f7ebb9f1fa75fac39a02ede6e4db0f256f53f8
andilemj <andilemjwara@gmail.com>
2022-09-05 11:03:44 +0000

    trying to make the header sticky

---
+++ stick.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {myFucntional()};
+
+
+var header = document.getElementById("myHeader");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+}
